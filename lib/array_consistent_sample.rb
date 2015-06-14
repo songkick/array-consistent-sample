551da41b83d006b8d062b6c84295a2d8a1a777d5
Robin Tweedie <robin@songkick.com>
2015-06-14 18:34:21 +0100

    correct required ruby version specification

--- lib/array_consistent_sample.rb
+++ lib/array_consistent_sample.rb
@@ -1,5 +1,7 @@
-require 'backports/1.9.1/array/sample' if RUBY_VERSION < '1.9.1'
-require 'backports/1.9.2/random' if RUBY_VERSION < '1.9.2' && !defined?(::Random)
+if RUBY_VERSION < '1.9.3'
+  require 'backports/1.9.1/array/sample'
+  require 'backports/1.9.2/random'
+end
 require 'digest/sha1'
 
 class Array
